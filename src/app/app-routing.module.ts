f664ba6b8649fd1bf2e3cc108b8321917dcb07f9
Talita Beneves <talitabeneves15@gmail.com>
2023-04-22 02:45:31 -0300

    feat: implementando auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,28 @@ import { CadastroComponent } from './cadastro/cadastro.component';
 import { LoginComponent } from './login/login.component';
 import { MetaDetalhesComponent } from './meta-detalhe/meta-detalhes.component';
 import { MetaHomeComponent } from './meta-home/meta-home.component';
+import { MeusAtivosComponent } from './meus-ativos/meus-ativos.component';
+import { AuthGuard } from 'src/core/guard/auth.guard';
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'cadastro', component: CadastroComponent },
-  { path: 'metas-home', component: MetaHomeComponent },
-  { path: 'meta-detalhe/:id', component: MetaDetalhesComponent },
+  {
+    path: 'metas-home',
+    component: MetaHomeComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'meta-detalhe/:id',
+    component: MetaDetalhesComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'meus-ativos',
+    component: MeusAtivosComponent,
+    canActivate: [AuthGuard],
+  },
   { path: '**', component: LoginComponent },
+  // { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
 ];
 
 @NgModule({
