86371d2cae817c9341368378c53027e87c90cf38
Talita Beneves <talitabeneves15@gmail.com>
2023-04-22 02:44:29 -0300

    feat: criando logout + fix rotas + fix style

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
   activeItem: string;
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit() {}
+
+  logout() {
+    localStorage.removeItem('usuario');
+    this.router.navigateByUrl('/login');
+  }
+
+  showMenu(): boolean {
+    return this.router.url !== '/login';
+  }
 }
