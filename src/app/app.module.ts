e4af67826a8ab1d7659ee16c77efd4c3676f4c06
Talita Beneves <talitabeneves15@gmail.com>
2023-04-22 02:48:55 -0300

    feat: imports + style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
+import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTabsModule } from '@angular/material/tabs';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { NgxMaskModule } from 'ngx-mask';
 import { ToastrModule } from 'ngx-toastr';
@@ -17,15 +17,16 @@ import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CadastroModule } from './cadastro/cadastro.module';
 import { LoginModule } from './login/login.module';
+import { MenuComponent } from './menu/menu.component';
 import { DialogMetaDetalheComponent } from './meta-detalhe/components/dialog-meta-detalhe/dialog-meta-detalhe.component';
 import { MetaDetalhesComponent } from './meta-detalhe/meta-detalhes.component';
 import { DialogMetaHomeComponent } from './meta-home/components/dialog-meta-home/dialog-meta-home.component';
 import { MetasAndamentoComponent } from './meta-home/components/metas-andamento/metas-andamento.component';
 import { MetasConcluidasComponent } from './meta-home/components/metas-concluidas/metas-concluidas.component';
 import { MetaHomeComponent } from './meta-home/meta-home.component';
-import { CadastroModule } from './cadastro/cadastro.module';
-import { MenuComponent } from './menu/menu.component';
+import { MeusAtivosModule } from './meus-ativos/meus-ativos.module';
 
 @NgModule({
   declarations: [
@@ -51,6 +52,7 @@ import { MenuComponent } from './menu/menu.component';
     ReactiveFormsModule,
     MatDatepickerModule,
     MatNativeDateModule,
+    MeusAtivosModule,
     MatTabsModule,
     NgxMaskModule.forRoot({
       dropSpecialCharacters: true,
