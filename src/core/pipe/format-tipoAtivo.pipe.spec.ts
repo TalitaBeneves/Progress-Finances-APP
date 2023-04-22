e7e2a94b3454d3d51f31fafdfe36105de38545fe
Talita Beneves <talitabeneves15@gmail.com>
2023-04-22 02:46:32 -0300

    feat: pipe para formatacao de tipo ativo

---
+++ src/core/pipe/format-tipoAtivo.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { FormatTipoAtivoPipe } from './format-tipoAtivo.pipe';
+
+describe('Pipe: FormatTipoAtivoe', () => {
+  it('create an instance', () => {
+    let pipe = new FormatTipoAtivoPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/core/pipe/format-tipoAtivo.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'formatTipoAtivo',
+})
+export class FormatTipoAtivoPipe implements PipeTransform {
+  transform(value: any, args?: any): any {
+    if (value == 0) return 'Ações';
+    if (value == 1) return 'Fundos Imobiliários';
+    if (value == 3) return 'Renda Fixa';
+  }
+}
